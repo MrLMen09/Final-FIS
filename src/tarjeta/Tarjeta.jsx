c9ad5f2799b3a67ce36fb99c1f66ee300d97f656
ccmendezt <camilomendez1903@gmail.com>
2022-03-28 15:50:31 -0500

    Botones de cantidad

--- src/tarjeta/Tarjeta.jsx
+++ src/tarjeta/Tarjeta.jsx
@@ -3,31 +3,42 @@ import ReactDOM from 'react-dom';
 import './Tarjeta.css';
 import Image from 'react-bootstrap/Image';
 
-function Tarjeta()
-{
-    
-    return(
-        <div id = "contenedorTarjeta">
-            <div id = "atras">
+function Tarjeta() {
+
+    return (
+        <div id="contenedorTarjeta">
+            <div id="atras">
 
             </div>
-            <div id = "adelante">
-                <div id = "imagen">
-                    <img src="../media/producto.png" alt="" id = "imagen-tarjeta"/>
+            <div id="adelante">
+                <div id="imagen">
+                    <img src="../media/producto.png" alt="" id="imagen-tarjeta" />
                 </div>
                 <hr />
-                <div id = "info">
+                <div id="info">
                     <h3>Producto</h3>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
                 </div>
-                <hr style={{color: "greenyellow"}}/>
-                <div id = "botones">
-                    <button id = "agregar" className='botonesTarjeta'>Agregar al Carrito</button>
-                    <button id = "comprar" className='botonesTarjeta'>Comprar Ahora</button>
+                <hr style={{ color: "greenyellow" }} />
+                <div id="botones">
+                    <button id="agregar" className='botonesTarjeta'>Agregar al Carrito</button>
+                    <button id="comprar" className='botonesTarjeta'>Comprar Ahora</button>
+                    <div className="row">
+                    <div className="col-lg-4 info1">
+                        <button type="button" className="btn btn-dark quantity">-</button>
+                    </div>
+                    <div className="col-lg-4 info2">
+                        <input type="text" className='i_quantity' min="0" defaultValue="0" disabled />
+                    </div>
+                    <div className="col-lg-4 info3">
+                        <button type="button" className="btn btn-dark quantity" >+</button>
+                    </div>
+                </div>
                 </div>
                 
+
             </div>
-        </div>    
+        </div>
     );
 
 }
